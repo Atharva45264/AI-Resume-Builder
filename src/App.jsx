77c5e77e5a04e3107cca96ac82ac01742e2b5f09
Atharva45 <atharvaphanse403@gmail.com>
2025-08-05 00:08:46 +0530

    working on header

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Navigate, Outlet } from 'react-router-dom'
 import { useUser } from '@clerk/clerk-react'
+import Header from './components/ui/custom/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
 
   return (
     <>
+    <Header/>
     <Outlet/>
     </>
   )
